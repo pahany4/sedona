f3a24ab768a13b0ec68d5228b25d84a6340d2e01
pahany4 <pahan_malysh@mail.ru>
2020-07-09 21:00:50 +0700

    анимация меню

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,31 @@
-var menuVisible = document.querySelectorAll('.menu-visible');
+var menuVisible = document.querySelector('nav ul');
 var screenSize = window.innerWidth;
 var burgerHid = document.querySelector('.menu-icon');
 
 function hideMenu() {
     if (screenSize < 768) {
         burgerHid.classList.remove('menu-icon_hid');
-        for (i = 0; i < menuVisible.length; i++) {
-            menuVisible[i].classList.add("menu-hid");
-        }
+        menuVisible.classList.add('menu-hid');
     }
 }
 hideMenu();
+var clk = 0;
 
 function menuOpen() {
     burgerHid.addEventListener('click',
         function (evt) {
             evt.preventDefault;
             this.classList.toggle('menu-icon_activ');
-            for (i = 0; i < menuVisible.length; i++) {
-                menuVisible[i].classList.toggle("menu-hid");
+            if (clk % 2 == 0) {
+                menuVisible.classList.remove('menu-hid');
+                menuVisible.classList.add('menu-open');
+                menuVisible.classList.remove('menu-close');
+            } else {
+                menuVisible.classList.remove('menu-open');
+                menuVisible.classList.add('menu-close');
+                menuVisible.classList.add('menu-hid')
             }
+            clk++;
         }
     )
 }
